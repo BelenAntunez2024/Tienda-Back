1a61ed050d25bd40285d3aca9eaae24ddbef0fc9
BelenAntunez2024 <beluantunez99@gmail.com>
2025-10-14 17:32:29 -0300

    Conexion con la base de datos (Supabase)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsuariosModule } from './usuarios/usuarios.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      synchronize: true,
+      autoLoadEntities: true,
+    }),
+    UsuariosModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  onApplicationBootstrap() {
+    console.log("Conexion a la base de datos exitosa")
+  }
+}
+
