9114ab7c95d7e7e3dce4676087da1ceb65232676
MoreGutierrez <morelstuff2000@gmail.com>
2025-10-19 14:58:06 -0300

    creacion de DTO de Producto

--- src/producto/dto/producto.dto.ts
+++ src/producto/dto/producto.dto.ts
@@ -1,1 +1,15 @@
-export class ProductoDto {}
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+
+export class ProductoDto {
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  precio: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  stock: number;
+}
