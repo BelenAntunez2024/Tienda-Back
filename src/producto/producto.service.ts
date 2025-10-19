f95a3032dead81d6e48218b7b2670a0bf8cc2dbf
MoreGutierrez <morelstuff2000@gmail.com>
2025-10-19 18:51:34 -0300

    implementacion de los metodos en el service de producto

--- src/producto/producto.service.ts
+++ src/producto/producto.service.ts
@@ -1,25 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Producto } from './entities/producto.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 import { ProductoDto } from './dto/producto.dto';
 
 @Injectable()
 export class ProductoService {
-  create(productoDto: ProductoDto) {
-    return 'This action adds a new producto';
+  //el constructor inyecta el repositorio de Producto
+  constructor(
+    @InjectRepository(Producto)
+    //el injectRepository lo que hace es decirle a nest que quiero usar la tabla Producto
+    private productoRepository: Repository<Producto>,
+  ) {}
+
+  //metodos CRUD basicos
+  //obtener todos los productos
+  async findAll(): Promise<Producto[]> {
+    return this.productoRepository.find();
   }
 
-  findAll() {
-    return `This action returns all producto`;
+  //obtener un producto por id
+  async findOne(id: number): Promise<Producto|null> {//el null es por si no lo encuentra
+    return this.productoRepository.findOne({where: {id_producto: id}});
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} producto`;
+  //crear un nuevo producto y verificar si ya existe por nombre
+  async create(productoDto: ProductoDto): Promise<Producto> {
+    const existente = await this.productoRepository.findOne({
+      where: {nombre: productoDto.nombre}
+    });
+    if (existente) {
+      throw new NotFoundException(`El producto ${productoDto.nombre} ya existe.`);
+    }
+    const producto = await this.productoRepository.create(productoDto);
+    return this.productoRepository.save(producto);
   }
 
-  update(id: number, productoDto: ProductoDto) {
-    return `This action updates a #${id} producto`;
+  //actualizar un producto y verificar si existe
+  async update(id: number, productoDto: ProductoDto): Promise<Producto> {
+    const producto = await this.productoRepository.findOne({
+      where: {id_producto: id}
+    });
+    if (!producto) {
+      throw new NotFoundException(`El producto con id ${id} no fue encontrado.`);
+    }
+    const productoActualizado = this.productoRepository.merge(producto, productoDto);
+    return this.productoRepository.save(productoActualizado);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} producto`;
+  //eliminar un producto
+  async remove(id: number) {
+    await this.productoRepository.delete(id);
   }
 }
